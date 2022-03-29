bfc4fab39a5dc6802c95a6c21bff82334935d39d
nova.dev <34773592+umutsatir@users.noreply.github.com>
2022-03-30 02:19:52 +0300

    29.03.2022

--- mysql_connection.py
+++ mysql_connection.py
@@ -36,7 +36,6 @@ def insertProducts(liste):
     connection = mysql.connector.connect(host="localhost", user="root", password="******", database="node-app")
     cursor = connection.cursor()
     sql = "INSERT INTO products(name,price,imageUrl,description) VALUES (%s,%s,%s,%s)" # %s yer tutucudur. Değişkenle değer vereceğiz.
-    values = liste
     cursor.executemany(sql,liste)
     try:
         connection.commit() # sorgunun çalışmasını sağlar. Sorgu DB'ye gönderilir.
@@ -62,3 +61,67 @@ while True:
         print("Kayıtlarınız veritabanına aktarılıyor...")
         insertProducts(list)
         break
+
+
+
+## VERİLERİ GETİRME ##
+
+def getProducts():
+    connection = mysql.connector.connect(host="localhost", user="root", password="******", database="node-app")
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM products") # yıldız işareti bütün kolonları çağırmak için kullanılır.
+    # SELECT name,price FROM products yazsaydık sadece name ve price kolonlarını getirirdi.
+    result = cursor.fetchall() # birden fazla kayıt aldığımızda commit yerine bunu kullanıyoruz.
+    result = cursor.fetchone() # sadece en baştaki kaydı getirir.
+    print(result)
+
+
+
+## KAYIT FİLTRELEME ##
+
+def getProductsWhere():
+    connection = mysql.connector.connect(host="localhost", user="root", password="******", database="node-app")
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM products WHERE id=1") # sadece id'si 1 olan satırları çağırır.
+    cursor.execute("SELECT * FROM products WHERE name='Samsung S6' AND price=3000") # ismi samsung S6 olan ve fiyatı 3000 olan kayıtları çağırır.
+    cursor.execute("SELECT * FROM products WHERE name='Samsung S6' OR price=3000") # ismi samsung S6 olan veya fiyatı 3000 olan kayıtları çağırır.
+    cursor.execute("SELECT * FROM products WHERE name LIKE '%Samsung%'") #name sütununda Samsung kelimesini içeren kayıtları çağırır.
+    cursor.execute("SELECT * FROM products WHERE name LIKE 'Samsung%'") #name sütununda başında Samsung kelimesi bulunan kayıtları çağırır.
+    cursor.execute("SELECT * FROM products WHERE name LIKE '%S6'") #name sütununda sonu S6 ile biten kayıtları çağırır.
+
+    result = cursor.fetchall()
+    print(result)
+
+
+
+## KAYIT SIRALAMA ##
+
+def getProductsOrderBy():
+    connection = mysql.connector.connect(host="localhost", user="root", password="******", database="node-app")
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM products ORDER BY name") # name kolonuna göre sıralama yapar.
+    cursor.execute("SELECT * FROM products ORDER BY name DESC") # name kolonuna göre sıralama yapar ama ters sıralar. (descending = DESC, ascending= ASC)
+
+    result = cursor.fetchall()
+
+
+
+## AGGREGATE (HESAPLAMA) FONKSİYONLARI ##
+
+def getProductsAgg():
+    connection = mysql.connector.connect(host="localhost", user="root", password="******", database="node-app")
+    cursor = connection.cursor()
+
+    sql = "SELECT COUNT(*) FROM products" # satır sayısını sayar.
+    sql = "SELECT AVG(price) FROM products" # fiyat alanının ortalamasını bulur.
+    sql = "SELECT SUM(price) FROM products" # fiyat alanının toplamını bulur.
+    sql = "SELECT MIN(price) FROM products" # minimum fiyatı bulur.
+    sql = "SELECT MAX(price) FROM products" # maksimum fiyatı bulur.
+    sql = "SELECT name FROM products WHERE price = (SELECT MAX(price) FROM products)" # maksimum fiyatın sahibi olan satırın ismini verir.
+
+    cursor.execute(sql)
+    result = cursor.fetchone()
+    print(result)
